166c0ab141fbc2cb2a3631b20b5f28d99309ae00
Chandler Ekins <chandler.ekins@bsci.com>
2019-05-28 18:31:11 -0400

    Began work on adding jenkins functions

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#### Description: This script is a source of functions for the 
+#### vpdiffs script. Functions related to locating directories
+#### and filenames as well as running squish tools.
 
-# This script contains functions for locating vp and screenshot
-# files that are use in other scripts
+# TODO  update variables names, add functions for using all tools, comment purpose of each function
 
-# TODO add description, update variables names, add functions for using all tools
 
+## Test Results functions	
 getTimestampDir () {
 	local tsDir=~/.squish/Test\ Results/	# directory storing all results
 	tsDir="$tsDir$(ls "$tsDir" | tail -1)"	# directory for most recent timestamp
@@ -39,8 +41,39 @@ getVPName () {
 	echo "$vpName"
 }
 
+
+
+## Generic functions
+
+# TODO make this take another argument to make it generic
 getMatchingVPs () {
 	local vpName=$1
 	local vpDir=$(getVPDir)
 	echo "$(find "$vpDir" -regex ".*/${vpName}\(_[0-9]+\)?$" -print)"	
 }
+
+
+
+## Jenkins functions
+getJenkinsDataDir() {
+	local ARCHIVE_DIR=$1
+	echo "${ARCHIVE_DIR}/archive/out/data/"
+}
+
+getJenkinsTestCaseName () {
+	local ARCHIVE_DIR=$1
+	local DATA_DIR=$(getJenkinsDataDir $ARCHIVE_DIR)
+	local TEST_CASE_NAME="$(ls "$DATA_DIR" | grep 'tst' | tail -1)" 
+	echo "$TEST_CASE_NAME"
+}
+
+
+getJenkinsScreenshotDir () {
+	local ARCHIVE_DIR=$1
+	local DATA_DIR=$(getJenkinsDataDir $ARCHIVE_DIR)
+	local TEST_CAST_NAME=$(getJenkinsTestCaseName $ARCHIVE_DIR)
+	local SS_DIR="${DATA_DIR}/${TEST_CASE_NAME}/${TEST_CASE_NAME}/"
+	echo "SS_DIR"
+}
+
+
